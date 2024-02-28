ff6646ed1c59560f2b23a653bb2e19bda4329cf0
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-28 10:07:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inlämningsuppgifterkapitel6._7
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal för att veta om det är ett primtal:");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine(Prim(tal));
+        }
+        static bool Prim(int tal)
+        {
+            int deltal = 0;
+            for (int i = 2; i < tal; i++)
+            {
+                if (tal % i == 0)
+                {
+                    deltal++;
+                }
+            }
+            if (deltal == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
